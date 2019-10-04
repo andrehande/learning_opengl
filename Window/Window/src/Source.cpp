24d2ce83ae1839548372d424b15d15b5cd28328c
André H. Eggehagen <andre.hande@hotmail.com>
2019-10-04 17:54:06 +0200

    Zooming and better usability

--- Window/Window/src/Source.cpp
+++ Window/Window/src/Source.cpp
@@ -1,6 +1,4 @@
-
 #define STB_IMAGE_IMPLEMENTATION
-
 
 /* GLEW*/
 #include <GL/glew.h>
@@ -23,20 +21,58 @@
 #include "Shaders.cpp"
 #include "stb_image.h"
 
-#define SCREEN_WIDTH 900	
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1920	
+#define SCREEN_HEIGHT 1080
 
 #define ASSERT(x) if(!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
 	x;\
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
-void processInput(GLFWwindow* window);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+float location = 15.0f;
+
+static unsigned int loadTexure(const char* path)
+{
+	unsigned int texture;
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+
+	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+		std::cout << "Failed to load texture" << std::endl;
+
+	stbi_image_free(data);
+
+	return texture;
+}
 
 static void GLClearError()
 {
 	while (glGetError() != GL_NO_ERROR);
 }
+
 
 static bool GLLogCall(const char* function, const char* file, int line)
 {
@@ -47,6 +83,7 @@ static bool GLLogCall(const char* function, const char* file, int line)
 	}
 	return true;
 }
+
 
 int main(void)
 {
@@ -125,19 +162,16 @@ int main(void)
 			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 	
-	glm::vec3 cubePositions[500];
-
+	glm::vec3 cubePositions[1000];
+		
 	for (int i = 0; i < sizeof(cubePositions) / sizeof(cubePositions[0]); i++)
 	{
-		float x = rand() % 41 + (-20);
-		float y = rand() % 41 + (-20);
-		float z = rand() % 41 + (-20);
-
-		std::cout << x << ":" << y << ":" << z << "\n";
+		float x = rand() % 45 + (-22);
+		float y = rand() % 45 + (-22);
+		float z = rand() % 45 + (-22);
 
 		cubePositions[i] = glm::vec3(x, y, z);
 	}
-
 
 	unsigned int VAO, VBO;
 
@@ -162,57 +196,36 @@ int main(void)
 	unsigned int shader = Shaders::CreateShader(source.VertexSource, source.FragmentSource);
 	GLCall(glUseProgram(shader));
 
-	unsigned int texture1;
-
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-	
-	// set texture wrapping to GL_REPEAT (default wrapping method)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	// load image, create texture and generate mipmaps
-	int width, height, nrChannels;
-
-	stbi_set_flip_vertically_on_load(true); 
-	unsigned char* data = stbi_load("res/img/crate.png", &width, &height, &nrChannels, 0);
-	
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-		std::cout << "Failed to load texture" << std::endl;
-	
-	stbi_image_free(data);
-	glEnable(GL_DEPTH_TEST);
+	unsigned int texture1 = loadTexure("res/img/crate35.png");
+	//unsigned int texture2 = loadTexure("res/img/crate1.png");
+	//unsigned int texture3 = loadTexure("res/img/crate2.png");
+	//unsigned int texture4 = loadTexure("res/img/crate3.png");
 
 	glm::mat4 projection = glm::perspective(glm::radians(60.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 	glUniformMatrix4fv(glGetUniformLocation(shader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
+
+	glEnable(GL_DEPTH_TEST);
 	while (!glfwWindowShouldClose(window))
 	{
+		glfwSetScrollCallback(window, scroll_callback);
+
+		float r = sin(glfwGetTime()) / 2.0f + 0.5f;
 		
-		float radius = 5.0f;
+		float radius = location;
 		float camX = sin(glfwGetTime() / 4) * radius;
 		float camZ = cos(glfwGetTime() / 4) * radius;
 		
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(r, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
 	
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
-
-		glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shader, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
 		glBindVertexArray(VAO);
 
@@ -224,6 +237,7 @@ int main(void)
 			model = glm::translate(model, cubePositions[i]);
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+			
 			glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_FALSE, glm::value_ptr(model));
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
@@ -240,8 +254,7 @@ int main(void)
 	return 0;
 }
 
-void processInput(GLFWwindow* window)
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+	location += (float) yoffset;
 }
